8e4092f08c31d780e09644de2ff8574a8438c284
spidunno <unnospid@gmail.com>
2023-03-19 22:15:15 -0400

    fixed drop button on stats page

--- Scripts/TemplateStat.gd
+++ Scripts/TemplateStat.gd
@@ -1,4 +1,4 @@
 extends NinePatchRect
 
 func _on_TextureButton_pressed():
-	get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().Drop(name)
+	get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().Drop(name)
