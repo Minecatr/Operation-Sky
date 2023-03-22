b180244bc11bd5a55f727ff86aadbae0603ac7f9
Minecat <emmett.hardiman@gmail.com>
2023-03-21 20:27:37 -0400

    ficksed miner

--- Scripts/Miner.gd
+++ Scripts/Miner.gd
@@ -9,6 +9,9 @@ func _on_area_3d_area_entered(area):
 		if area.is_in_group("hurtbox"):
 			if area.health > 0:
 				area.take_damage(1+upgrades.value["Damage Multi"],self)
+				target = null
+				$Area3D/CollisionShape3D2.set_deferred("disabled", true)
+				$Breaktimer.start()
 
 func collect_stat(statss):
 	for stat in statss:
@@ -26,14 +29,19 @@ func _on_timer_timeout():
 			for resourcetype in island.get_node("Resources").get_children():
 				for resource in resourcetype.get_children():
 					if target:
-						if global_position.distance_to(resource.position) < global_position.distance_to(target):
-							target = resource.position
-					else:
-						target = resource.position
+						if get_parent().global_position.distance_to(resource.global_position) < get_parent().global_position.distance_to(target):
+							target = resource.global_position
+					elif get_parent().global_position.distance_to(resource.global_position) < 20:
+						target = resource.global_position
 
 func _physics_process(_ddelta):
 	if $"..".placed and target:
 		look_at(target)
-		velocity = Vector3(1,0,0).rotated(Vector3.UP, rotation.y)
+		velocity = -global_transform.basis.z
+		#velocity = Vector3(0,1,0).rotated(Vector3.UP, rotation.y)
 		#velocity = velocity.rotated(Vector3.BACK, rotation.x)
 		move_and_slide()
+
+
+func _on_breaktimer_timeout():
+	$Area3D/CollisionShape3D2.set_deferred("disabled", false)
